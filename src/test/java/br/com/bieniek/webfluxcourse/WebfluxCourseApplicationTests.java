7a4f4ca8f861f302159004e4222b8123aac266d7
Bianeck <thiagobianeck@gmail.com>
2023-03-20 08:58:36 -0300

    Initial commit

---
+++ src/test/java/br/com/bieniek/webfluxcourse/WebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bieniek.webfluxcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
