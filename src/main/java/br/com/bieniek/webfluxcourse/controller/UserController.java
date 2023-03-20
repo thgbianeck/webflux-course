23e6ff55bd72d7b2d62c747b60a187251ea2250f
Bianeck <thiagobianeck@gmail.com>
2023-03-20 11:11:01 -0300

    Adição da Interface de Controlador de User

---
+++ src/main/java/br/com/bieniek/webfluxcourse/controller/UserController.java
@@ -0,0 +1,25 @@
+package br.com.bieniek.webfluxcourse.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UserController {
+
+    @PostMapping
+    ResponseEntity<Mono<Void>> save(@RequestBody UserRequest request);
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Mono<UserResponse>> findById(@PathVariable String id);
+
+    @GetMapping
+    ResponseEntity<Flux<UserResponse>> findAll();
+
+    @PatchMapping(value = "/{id}")
+    ResponseEntity<Mono<UserResponse>> update(@PathVariable String id, @RequestBody UserRequest request);
+
+    @DeleteMapping(value = "/{id}")
+    ResponseEntity<Mono<Void>> delete(@PathVariable String id);
+
+}
